563ccbae3d63ca87b6a8bcaf7ec4d2880eaab4a4
DerPenz <mike2407@t-online.de>
2022-11-08 16:01:10 +0100

    updated Navbar with styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { TbLetterCase } from 'react-icons/tb';
+import { themeChange } from 'theme-change';
 
 export default function Navbar() {
-  return (
-    <nav className='flex flex-row gap-2 justify-start p-2 bg-slate-800 text-white'>
-        <h2>Scrabble</h2>
-        <div className='ml-auto'>Theme</div>
-    </nav>
-  )
+	useEffect(() => {
+		themeChange(false);
+	}, []);
+
+	
+
+	return (
+		<nav className="flex flex-row gap-2 justify-start p-2 bg-base-300 items-center">
+			<TbLetterCase size={'2%'} className='hover:animate-ping border rounded-lg border-primary text-primary p-1'/>
+			<a href='/' className='font-bold text-2xl underline decoration-wavy hover:animate-pulse'>Scrabble</a>
+			<select defaultValue={localStorage.getItem('theme') || 'Pick a theme'} className='select ml-auto' data-choose-theme>
+				<option disabled >Pick a theme</option>
+				<option value="dark">Dark</option>
+				<option value="light">Light</option>
+				<option value="cupcake">Cupcake</option>
+				<option value="dracula">Dracula</option>
+				<option value="halloween">Halloween</option>
+				<option value="lofi">Lofi</option>
+				<option value="luxury">Luxury</option>
+				<option value="fantasy">Fantasy</option>
+				<option value="night">Night</option>
+				<option value="coffee">Coffee</option>
+				<option value="autumn">Autumn</option>
+				<option value="synthwave">Synthwave</option>
+			</select>
+		</nav>
+	);
 }
